eb218aefb12f8f7de39e0fe539816b9f4cb6afcd
Abenezer Daniel <aabenezzer@gmail.com>
2026-06-30 14:23:55 +0300

    setup flux ui

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,19 +1,29 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-        <title>{{ $title ?? config('app.name') }}</title>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <title>{{ $title ?? config('app.name') }}</title>
 
-        @livewireStyles
-    </head>
-    <body>
-        {{ $slot }}
-        <h1>Hello World</h1>
+    ...
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=inter:400,500,600&display=swap" rel="stylesheet" />
 
-        @livewireScripts
-    </body>
+
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+
+    @livewireStyles
+    @fluxAppearance
+</head>
+
+<body>
+    {{ $slot }}
+    <h1>Hello World</h1>
+
+    @livewireScripts
+    @fluxScripts
+</body>
+
 </html>
